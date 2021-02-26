31fa4899d5091874e67d4b6e24c5eaa48f081098
Dawid Kociubiński <dkociubinski@cpit.local>
2021-02-26 12:40:59 +0100

    pierwszy komit try

---
+++ products/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):   
+    title       =   models.CharField(max_length=200,null=False)
+    description =   models.TextField()
+    price       =   models.DecimalField(decimal_places=2, max_digits=1000)
+    summary     =   models.TextField(blank=True, default="this is my product buy it")
+    features    =   models.BooleanField()
