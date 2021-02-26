60f1ad1d500e19a8d535a4175828834a0d1c8c1a
daveoo-k <d.kociubinski@gmailcom>
2021-02-26 21:46:33 +0100

    dictionary to blocks

--- strony/views.py
+++ strony/views.py
@@ -8,5 +8,13 @@ def home_view(request,*args,**kwargs):
 def contact_view(request,*args,**kwargs):
     return render(request,"contacts.html",{})
 
+
 def about_view(request,*args,**kwargs):
-    return render(request,"about.html",{})
+    
+    my_dictionary={
+        "my_pos1":"tekst z pozycji pierwszej",
+        "my_pos2":"tekst z pozycji drugiej",
+        "my_pos3":"tekst z pozycji trzeciej",
+        "my_pos1_int":23,
+    }
+    return render(request,"about.html",my_dictionary)
